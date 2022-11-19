f3658aa49bd0f1a6b5f773e224791850705ba45a
Deep0054 <103152350+Deep0054@users.noreply.github.com>
2022-11-19 16:04:42 -0500

    Update wordguesser_game.rb

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,10 +1,9 @@
 class WordGuesserGame
 
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/wordguesser_game_spec.rb pass.
+ 
   attr_accessor :word, :guesses, :wrong_guesses
 
-  # Get a word from remote "random word" service
+  
 
   def initialize(word)
     @word = word
@@ -12,10 +11,7 @@ class WordGuesserGame
     @wrong_guesses = ""
   end
 
-  # You can test it by installing irb via $ gem install irb
-  # and then running $ irb -I. -r app.rb
-  # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
-  #  => "cooking"   <-- some random word
+  
   def self.get_random_word
     require 'uri'
     require 'net/http'
